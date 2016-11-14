ecbd0f92c6f56bca5cc8425d402de88a77ea1433
Shisharkata <kristiyanayvazov@gmail.com>
2016-11-14 21:52:32 +0200

    Update Shift.java

--- src/sample/Shift.java
+++ src/sample/Shift.java
@@ -4,55 +4,49 @@ package sample;
  * Created by Kristiyan Ayvazov on 11/12/2015.
  */
 public class Shift {
+    //attributes
     private String position;
     private String date;
     private int salary;
     private int hours;
-
-    public Shift(){
-        this.position = "";
-        this.date = "";
-        this.salary = 0;
-        this.hours = 0;
-    }
-
+    //get method
     public String getDate() {
         return date;
     }
-
+    //set method
     public void setDate(String date) {
         this.date = date;
     }
-
-    public Shift(String position,String date, int wage, int quantity){
+    // Shift constructor
+    public Shift( String position, String date, int wage, int quantity){
         this.position = position;
         this.date = date;
-
         this.salary = wage;
         this.hours = quantity;
     }
-
+    //get method
     public String getPosition() {
         return position;
     }
-
+    //set method
     public void setPosition(String position) {
         this.position = position;
     }
-
+    //get method
     public int getWage() {
         return salary;
     }
-
+    //set method
     public void setWage(int wage) {
         this.salary = wage;
     }
-
+    //get method
     public int getQuantity() {
         return hours;
     }
-
+    //set method
     public void setQuantity(int quantity) {
         this.hours = quantity;
     }
+
 }
